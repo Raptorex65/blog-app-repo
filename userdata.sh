9960ba21cffc40384cece0058a738750ff1ebd99
Raptorex65 <71727239+Raptorex65@users.noreply.github.com>
2021-12-06 00:52:06 +0100

    "Settings added"

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN='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'
+git clone https://$TOKEN@github.com/Raptorex65/blog-app-repo.git
+cd /home/ubuntu/blog-app-repo
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/blog-app-repo/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
